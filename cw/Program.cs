57b552ed2b4f8ed6ccbad9dc46f518c6b3f0ac6a
Jan Antczak <s32628@pjwstk.edu.pl>
2026-03-11 10:00:15 +0100

    Modified Program.cs to display numbers in an interesting way

--- cw/Program.cs
+++ cw/Program.cs
@@ -5,9 +5,14 @@ class Program
     static void Main(string[] args)
     {
         int number = 10;
-
+        char spc = ' ';
+        
         for (int i = 1; i <= number; i++)
         {
+            for (int j = 20; j >= 0; j-=i)
+            {
+                Console.Write(spc);
+            }
             Console.WriteLine(i);
         }
     }
