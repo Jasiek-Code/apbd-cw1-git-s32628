7e89308e06391c3ddee0ffdaa88c008e16ece678
Jan Antczak <s32628@pjwstk.edu.pl>
2026-03-11 09:53:37 +0100

    replaced single Console.WriteLine in line 8 with a for loop

--- cw/Program.cs
+++ cw/Program.cs
@@ -5,6 +5,10 @@ class Program
     static void Main(string[] args)
     {
         int number = 10;
-        Console.WriteLine(number);
+
+        for (int i = 1; i <= number; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
